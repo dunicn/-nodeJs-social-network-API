c798105cc78b3f494e472a92761f30d123e95b22
dunicn <nemanjazdunic@gmail.com>
2021-08-03 18:04:22 +0200

    added socket.io on the server

--- app.js
+++ app.js
@@ -69,7 +69,11 @@ mongoose
     'mongodb+srv://Nemanja:JCuiIMmqKSyOsxaO@cluster0.x6zjo.mongodb.net/messages'
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', (socket) => {
+      console.log('client connected');
+    });
   })
   .catch((err) => {
     console.log(err);
